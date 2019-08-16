a07bfe2cf04d1167b865dbbdc191f167cdb506d8
roberto-arreortua <robertoarreortua@gmail.com>
2019-08-16 11:35:19 -0500

    make it better

--- ZHidra/urls.py
+++ ZHidra/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView
+from django.urls import reverse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
